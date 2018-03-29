be0324765ccc263495c7a8ca23a4f80d3abc7366
ronnyhdez <ronny.hernandezm@gmail.com>
2018-03-28 19:56:53 -0600

    actualizacion script

--- scripts/script_tutorial.R
+++ scripts/script_tutorial.R
@@ -43,5 +43,59 @@ head(data)
 
 # ---- 3. Análisis estadísticos con datos de fechas ----
 
+data$Rdate <- as.POSIXlt(data$Rdate)
+modelo <- lm(arr_delay ~ Rdate, data = data)
 
+data$Rdate <- as.POSIXct(data$Rdate)
+
+modelo <- lm(arr_delay ~ Rdate, data = data)
+summary(modelo)
+
+anova <- aov(arr_delay ~ Rdate, data = data)
+summary(anova)
+
+#convertir a continuo
+data$Rdate <- as.POSIXct(data$Rdate) 
+
+## ---- 3.1 Nombre completo del día ----
+ejemplo <- data[1,1]
+ejemplo
+
+weekdays(ejemplo)
+
+## ---- 3.2 Otros formatos ----
+otras_fechas <- c("2feb2016","18jun1990","7nov1995")
+strptime(otras_fechas,"%d%b%Y")
+
+# ---- 4. Cálculos con el tiempo ----
+as.difftime(otras_fechas, "%d%b%Y")
+
+
+# vamos a generar dos fechas:
+fecha_1 <- as.POSIXlt("2018-01-01")
+fecha_2 <- as.POSIXlt("2017-01-02")
+
+fecha_1 + 100000
+
+fecha_1 - 100000
+
+fecha_1 - fecha_2
+
+fecha_1 <= fecha_2
+
+difftime("2018-01-02","2017-12-01")
+
+tiempo_1 <- as.difftime("12:00:00")
+tiempo_2 <- as.difftime("17:20:00")
+tiempo_2 - tiempo_1
+
+# ---- 5. Generación de secuencias con tiempo ----
+
+seq(as.POSIXlt("2018-01-01"), as.POSIXlt("2018-01-10"), "1 day")
+
+seq(as.POSIXlt("2018-01-01"), as.POSIXlt("2018-02-01"), "1 weeks")
+
+seq(as.POSIXlt("2018-01-01"),as.POSIXlt("2018-01-02"),8000)
+
+seq(as.POSIXlt("2018-01-01"), by = "weeks", length = 7)
 
